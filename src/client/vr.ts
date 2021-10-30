c700562befc933b7aa644690c7c9d65f4d8be8e2
Luke McRedmonde <lmcredmond@kefron.com>
2021-10-30 23:10:58 +0100

    lets just rotate y

--- src/client/vr.ts
+++ src/client/vr.ts
@@ -297,17 +297,17 @@ class App {
     var y = this.dolly.rotation.y;
 
     //reset camera's x rotation.
-    this.dolly.rotateX(-x);
+    //this.dolly.rotateX(-x);
     //this.dolly.rotateY(-y);
 
     //rotate camera on y axis
     //this.dolly.rotateY(y+cleany);
 
     //check if we are trying to look to high or too low
-    if (Math.abs(cleanx + x) > Math.PI / 2 - 0.05) 
-      this.dolly.rotateX(x);
-    else 
-      this.dolly.rotateX(x + cleanx);
+    // if (Math.abs(cleanx + x) > Math.PI / 2 - 0.05) 
+    //   this.dolly.rotateX(x);
+    // else 
+    //   this.dolly.rotateX(x + cleanx);
 
     if (Math.abs(cleany + y) > Math.PI / 2 - 0.05) 
       this.dolly.rotateY(y);
