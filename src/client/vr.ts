1b58a0eb102aad46ec91c607b69270d4c5340ce9
Luke McRedmonde <lmcredmond@kefron.com>
2021-10-30 03:31:33 +0100

    just x access

--- src/client/vr.ts
+++ src/client/vr.ts
@@ -295,17 +295,19 @@ class App {
 
     //reset camera's x rotation.
     this.dolly.rotateX(-x);
-    this.dolly.rotateY(-y);
+    //this.dolly.rotateY(-y);
 
     //rotate camera on y axis
     //this.dolly.rotateY(y+cleany);
 
     //check if we are trying to look to high or too low
-    if (Math.abs(cleanx + x) > Math.PI / 2 - 0.05) this.dolly.rotateX(x);
-    else this.dolly.rotateX(x + cleanx);
+    if (Math.abs(cleanx + x) > Math.PI / 2 - 0.05) 
+      this.dolly.rotateX(x);
+    else 
+      this.dolly.rotateX(x + cleanx);
 
-    if (Math.abs(cleany + y) > Math.PI / 2 - 0.05) this.dolly.rotateY(y);
-    else this.dolly.rotateY(y + cleany);
+    //if (Math.abs(cleany + y) > Math.PI / 2 - 0.05) this.dolly.rotateY(y);
+    //else this.dolly.rotateY(y + cleany);
 
     //reset z rotation. Floating point operations might change z rotation during the above operations.
     this.dolly.rotation.z = 0;
